68455b71333af0fec0863a8b064c0592206b1680
MK-CIAN <cian.mck20@gmail.com>
2024-01-30 12:56:50 +0000

    Cleaning Up Routes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
-import { Observable, Subscription } from 'rxjs';
+import {Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -12,7 +14,7 @@ export class HeaderComponent implements OnInit{
   showAddTask: boolean = false;
   subscription: Subscription = new Subscription();
 
-  constructor(private uiService:UiService) {
+  constructor(private uiService:UiService, private router: Router) {
     this.subscription = this.uiService
     .onToggle()
     .subscribe((value) => (this.showAddTask = value));
@@ -22,5 +24,9 @@ export class HeaderComponent implements OnInit{
 
   toggleAddTask() {
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
